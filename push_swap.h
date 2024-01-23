0ed8bee3c2aa8a0c2eb0fe6c02bb5336b2329790
iamjunhyeong <dddodo447@gmail.com>
2024-01-23 22:27:02 +0900

    modify headerfile

--- push_swap.h
+++ push_swap.h
@@ -6,16 +6,16 @@
 /*   By: junhyeop <junhyeop@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/20 18:51:49 by junhyeop          #+#    #+#             */
-/*   Updated: 2024/01/21 01:01:25 by junhyeop         ###   ########.fr       */
+/*   Updated: 2024/01/23 20:13:33 by junhyeop         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
-
-#include <stdlib.h>
-#include <unistd.h>
-#include <stdio.h>
+# define ERROR -1
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
 
 typedef struct s_list 
 {
@@ -29,5 +29,8 @@ typedef struct s_node
 	int				size;
 	struct s_list	*head;
 }	t_node;
+
+char	**ft_split(char const *s, char c);
+int		ft_atoi(const char *str);
 
 #endif
