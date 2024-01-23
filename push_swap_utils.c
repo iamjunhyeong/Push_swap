845b96507c5f10bcd2994996addb7c915a6c0128
iamjunhyeong <dddodo447@gmail.com>
2024-01-23 22:26:38 +0900

    feat five_list_sort

--- push_swap_utils.c
+++ push_swap_utils.c
@@ -6,7 +6,7 @@
 /*   By: junhyeop <junhyeop@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/20 18:51:38 by junhyeop          #+#    #+#             */
-/*   Updated: 2024/01/21 08:42:06 by junhyeop         ###   ########.fr       */
+/*   Updated: 2024/01/23 22:07:42 by junhyeop         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -149,7 +149,7 @@ void	swap_s(t_node *a_node, t_node *b_node)
 	swap(a_node);
 	swap(b_node);
 }
-void	push(t_node *a_node, t_node *b_node)
+void	push_A(t_node *a_node, t_node *b_node)
 {
 	t_list	*top;
 	t_list	*tmp;
@@ -161,9 +161,10 @@ void	push(t_node *a_node, t_node *b_node)
 	top->next->prev = top->prev;
 	b_node->size--;
 	if (a_node->size == 0)
-	{
+	{	
 		a_node->head = top;
-		b_node = NULL;
+		top->next = top;
+		top->prev = top;
 	}
 	else if (a_node->size == 1)
 	{
@@ -183,39 +184,40 @@ void	push(t_node *a_node, t_node *b_node)
 	a_node->size++;
 }
 
-// void	push_b(t_node *a_node, t_node *b_node)
-// {
-// 	t_list	*top;
-// 	t_list	*tmp;
+void	push_B(t_node *a_node, t_node *b_node)
+{
+	t_list	*top;
+	t_list	*tmp;
 	
-// 	if (a_node == NULL)
-// 		return ;
-// 	top = a_node->head->prev;
-// 	top->prev->next = top->next;
-// 	top->next->prev = top->prev;
-// 	a_node->size--;
-// 	if (b_node->size == 0)
-// 	{
-// 		b_node->head = top;
-// 		a_node = NULL;
-// 	}
-// 	else if (b_node->size == 1)
-// 	{
-// 		b_node->head->next = top;
-// 		b_node->head->prev = top;
-// 		top->next = b_node->head;
-// 		top->prev = b_node->head;
-// 	}
-// 	else if (b_node->size >= 2)
-// 	{		
-// 		tmp = b_node->head->prev;
-// 		tmp->next = top;
-// 		b_node->head->prev = top;
-// 		top->next = b_node->head;
-// 		top->prev = tmp;
-// 	}
-// 	b_node->size++;
-// }
+	if (a_node == NULL)
+		return ;
+	top = a_node->head->prev;
+	top->prev->next = top->next;
+	top->next->prev = top->prev;
+	a_node->size--;
+	if (b_node->size == 0)
+	{
+		b_node->head = top;
+		top->next = top;
+		top->prev = top;
+	}
+	else if (b_node->size == 1)
+	{
+		b_node->head->next = top;
+		b_node->head->prev = top;
+		top->next = b_node->head;
+		top->prev = b_node->head;
+	}
+	else if (b_node->size >= 2)
+	{		
+		tmp = b_node->head->prev;
+		tmp->next = top;
+		b_node->head->prev = top;
+		top->next = b_node->head;
+		top->prev = tmp;
+	}
+	b_node->size++;
+}
 
 void	rotate(t_node *node)
 {
@@ -250,7 +252,11 @@ void	rev_rotate_r(t_node *a_node, t_node *b_node)
 	if (b_node->size >= 2)
 		b_node->head = b_node->head->next;
 }
-
+void	two_list_sort(t_node *node)
+{
+	if (node->head->data < node->head->next->data)
+		rotate(node);
+}
 
 
 // void	push_B(t_node a_node, t_node b_node)
@@ -272,6 +278,7 @@ void	rev_rotate_r(t_node *a_node, t_node *b_node)
 // 		if (pb > top)
 // 			pb(a_node, b_node);
 // 	}
+// 	write (1, "pb\n", 3);
 // }
 
 void	three_list_sort(t_node *node)
@@ -290,23 +297,102 @@ void	three_list_sort(t_node *node)
 		swap(node);
 }
 
-int main()
+int	set_arr(int ac, char **av, t_node *node)
+{
+	t_list	*tmp;
+	int		 cnt;
+	// arr = (int *)malloc(sizeof(int) * ac);
+	// if (!arr)
+	// 	return (-1);
+	cnt = 0;
+	while (ac-- > 1)
+	{
+		tmp = ft_lstnew(ft_atoi(av[ac]));
+		if (!tmp)
+			return (-1);
+		ft_lstadd_back(node, tmp);
+		cnt++;
+	}
+	return (cnt);
+}
+void	a_find_location(t_node *node, t_list *a, t_list *tmp)
+{
+	a = node->head->prev;
+	if (tmp->data > a->data)
+		return ;
+	a = a->prev;
+	if (tmp->prev->data > a->data)
+		swap(node);
+	else if (tmp->prev->prev->data > a->prev->data)
+	{
+		rev_rotate(node);
+		swap(node);
+		rotate(node);
+		rotate(node);
+	}
+	else
+		rev_rotate(node);
+}
+
+void	b_find_location(t_node *node, t_list *b, t_list *tmp)
+{
+	b = node->head->prev;
+	if (tmp->data > b->data)
+		return ;
+	b = b->prev;
+	if (tmp->prev->data > b->data)
+		swap(node);
+	else if (tmp->prev->prev->data > b->data)
+	{
+		swap(node);
+		rotate(node);
+		swap(node);
+		rev_rotate(node);
+	}
+	else if (tmp->prev->prev->prev->data > b->data)
+	{
+		rev_rotate(node);
+		swap(node);
+		rotate(node);
+		rotate(node);
+	}
+	else
+		rev_rotate(node);
+}
+
+void	five_list_sort(t_node *a_node, t_node *b_node)
+{
+	t_list	*tmp;
+
+	push_B(a_node, b_node);
+	push_B(a_node, b_node);
+	three_list_sort(a_node);
+	tmp = a_node->head->prev;
+	push_A(a_node, b_node);
+	a_find_location(a_node, a_node->head->prev, tmp);
+	tmp = a_node->head->prev;
+	push_A(a_node, b_node);
+	b_find_location(a_node, a_node->head->prev, tmp);
+}
+
+int main(int argc, char **argv)
 {
 	t_node	*a_node;
 	t_node	*b_node;
+	int		arr_size;
 
+	if (argc < 2)
+		return (ERROR);
 	a_node = init_node();
 	b_node = init_node();
-	// ft_lstadd_back(a_node, ft_lstnew(8));
-	// ft_lstadd_back(a_node, ft_lstnew(5));
-	// ft_lstadd_back(a_node, ft_lstnew(6));
-	ft_lstadd_back(a_node, ft_lstnew(2));
-	ft_lstadd_back(a_node, ft_lstnew(3));
-	ft_lstadd_back(a_node, ft_lstnew(1));
-	check_pivot()
-	ft_lstprint(a_node);
-	three_list_sort(a_node);
-	ft_lstprint(a_node);
 	
-	
+	arr_size = set_arr(argc, argv, a_node);
+	if (arr_size == -1)
+		return (ERROR);
+
+	ft_lstprint(a_node);
+	// push_B(a_node, b_node);
+	five_list_sort(a_node, b_node);
+	ft_lstprint(a_node);
+	ft_lstprint(b_node);
 }
