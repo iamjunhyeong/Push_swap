4b24b071136771468dd0a89e1fac9502247a8725
iamjunhyeong <dddodo447@gmail.com>
2024-01-24 22:56:02 +0900

    feat sorting

--- push_swap_utils.c
+++ push_swap_utils.c
@@ -6,11 +6,15 @@
 /*   By: junhyeop <junhyeop@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/20 18:51:38 by junhyeop          #+#    #+#             */
-/*   Updated: 2024/01/23 22:07:42 by junhyeop         ###   ########.fr       */
+/*   Updated: 2024/01/24 22:37:22 by junhyeop         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+
+//////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////
 
 int	ft_lstadd_back(t_node *node, t_list *new)
 {
@@ -102,7 +106,7 @@ t_node	*init_node()
 	return (node);
 }
 
-void	swap(t_node *node)
+void	swap_a(t_node *node)
 {
 	t_list	*a;
 	t_list	*b;
@@ -121,33 +125,36 @@ void	swap(t_node *node)
 	a->next = b;
 	b->next = node->head;
 	b->prev = a;
+	write(1, "sa\n", 3);
 }
 
-// void	swap_b(t_node *node)
-// {
-// 	t_list	*a;
-// 	t_list	*b;
+void	swap_b(t_node *node)
+{
+	t_list	*a;
+	t_list	*b;
 	
-// 	if (node == NULL || node->size == 1)
-// 		return ;	
-// 	a = node->head->prev;
-// 	b = a->prev;
+	if (node == NULL || node->size == 1)
+		return ;	
+	a = node->head->prev;
+	b = a->prev;
 
-// 	if (node->size >= 3)
-// 	{
-// 		a->next->prev = b;
-// 		b->prev->next = a;
-// 	}
-// 	a->prev = b->prev;
-// 	a->next = b;
-// 	b->next = node->head;
-// 	b->prev = a;
-// }
+	if (node->size >= 3)
+	{
+		a->next->prev = b;
+		b->prev->next = a;
+	}
+	a->prev = b->prev;
+	a->next = b;
+	b->next = node->head;
+	b->prev = a;
+	write(1, "sb\n", 3);
+}
 
 void	swap_s(t_node *a_node, t_node *b_node)
 {
-	swap(a_node);
-	swap(b_node);
+	swap_a(a_node);
+	swap_b(b_node);
+	write(1, "ss\n", 3);
 }
 void	push_A(t_node *a_node, t_node *b_node)
 {
@@ -182,6 +189,7 @@ void	push_A(t_node *a_node, t_node *b_node)
 		top->prev = tmp;
 	}
 	a_node->size++;
+	write (1, "pa\n", 3);
 }
 
 void	push_B(t_node *a_node, t_node *b_node)
@@ -217,45 +225,68 @@ void	push_B(t_node *a_node, t_node *b_node)
 		top->prev = tmp;
 	}
 	b_node->size++;
+	write (1, "pb\n", 3);
 }
 
-void	rotate(t_node *node)
+void	rotate_a(t_node *node)
 {
 	if (node->size >= 2)
+	{
 		node->head = node->head->prev;
+		write (1, "ra\n", 3);
+	}
 }
 
-// void	rotate_b(t_node *node)
-// {
-// 	if (node->size >= 2)
-// 		node->head = node->head->next;
-// }
+void	rotate_b(t_node *node)
+{
+	if (node->size >= 2)
+	{
+		node->head = node->head->next;
+		write (1, "rb\n", 3);
+	}
+}
 
 void	rotate_r(t_node *a_node, t_node *b_node)
 {
-	if (a_node->size >= 2)
+	if (a_node->size >= 2 && b_node->size >= 2)
+	{
 		a_node->head = a_node->head->prev;
-	if (b_node->size >= 2)
 		b_node->head = b_node->head->prev;
+		write (1, "rr\n", 3);
+	}
 }
 
-void	rev_rotate(t_node *node)
+void	rev_rotate_a(t_node *node)
 {
 	if (node->size >= 2)
+	{
 		node->head = node->head->next;
+		write (1, "rra\n", 3);
+	}
+}
+
+void	rev_rotate_b(t_node *node)
+{
+	if (node->size >= 2)
+	{
+		node->head = node->head->next;
+		write (1, "rrb\n", 3);
+	}
 }
 
 void	rev_rotate_r(t_node *a_node, t_node *b_node)
 {
-	if (a_node->size >= 2)
+	if (a_node->size >= 2 && b_node->size >= 2)
+	{
 		a_node->head = a_node->head->next;
-	if (b_node->size >= 2)
 		b_node->head = b_node->head->next;
+		write (1, "rrr\n", 3);
+	}
 }
 void	two_list_sort(t_node *node)
 {
 	if (node->head->data < node->head->next->data)
-		rotate(node);
+		rotate_a(node);
 }
 
 
@@ -286,15 +317,15 @@ void	three_list_sort(t_node *node)
 	if (node->head->next->data > node->head->prev->data)
 	{
 		if (node->head->data < node->head->next->data)
-			rev_rotate(node);
+			rev_rotate_a(node);
 	}
 	else
 	{
 		if (node->head->data < node->head->prev->data)
-			rotate(node);
+			rotate_a(node);
 	}
 	if (node->head->prev->data > node->head->next->data)
-		swap(node);
+		swap_a(node);
 }
 
 int	set_arr(int ac, char **av, t_node *node)
@@ -322,16 +353,16 @@ void	a_find_location(t_node *node, t_list *a, t_list *tmp)
 		return ;
 	a = a->prev;
 	if (tmp->prev->data > a->data)
-		swap(node);
+		swap_a(node);
 	else if (tmp->prev->prev->data > a->prev->data)
 	{
-		rev_rotate(node);
-		swap(node);
-		rotate(node);
-		rotate(node);
+		rev_rotate_a(node);
+		swap_a(node);
+		rotate_a(node);
+		rotate_a(node);
 	}
 	else
-		rev_rotate(node);
+		rev_rotate_a(node);
 }
 
 void	b_find_location(t_node *node, t_list *b, t_list *tmp)
@@ -341,23 +372,23 @@ void	b_find_location(t_node *node, t_list *b, t_list *tmp)
 		return ;
 	b = b->prev;
 	if (tmp->prev->data > b->data)
-		swap(node);
+		swap_a(node);
 	else if (tmp->prev->prev->data > b->data)
 	{
-		swap(node);
-		rotate(node);
-		swap(node);
-		rev_rotate(node);
+		swap_a(node);
+		rotate_a(node);
+		swap_a(node);
+		rev_rotate_a(node);
 	}
 	else if (tmp->prev->prev->prev->data > b->data)
 	{
-		rev_rotate(node);
-		swap(node);
-		rotate(node);
-		rotate(node);
+		rev_rotate_a(node);
+		swap_a(node);
+		rotate_a(node);
+		rotate_a(node);
 	}
 	else
-		rev_rotate(node);
+		rev_rotate_a(node);
 }
 
 void	five_list_sort(t_node *a_node, t_node *b_node)
@@ -375,6 +406,253 @@ void	five_list_sort(t_node *a_node, t_node *b_node)
 	b_find_location(a_node, a_node->head->prev, tmp);
 }
 
+void	find_pibot(t_node *a_node, int *pibot)
+{
+	t_list	*tmp;
+
+	tmp = a_node->head;
+	if (tmp->data > tmp->prev->data)
+	{
+		pibot[0] = tmp->data;
+		pibot[1] = tmp->prev->data;
+	}
+	else
+	{
+		pibot[0] = tmp->prev->data;
+		pibot[1] = tmp->data;
+	}
+
+
+	// t_list *tmp;
+	// int		i;
+	// int		j;
+	// int		min;
+
+	// printf("%d %d \n\n", fir ,sec);
+	// i = 0;
+	// j = 0;
+	// min = 0;
+	// tmp = a_node->head;
+	// while (a_node->size > j++)
+	// {
+	// 	if (tmp->data > min)
+	// 	{
+	// 		min = tmp->data;
+	// 		i++;
+	// 	}
+	// 	if (i == fir)
+	// 		pibot[0] = min;
+	// 	if (i == sec)
+	// 	{
+	// 		pibot[1] = min;
+	// 		break ;
+	// 	}
+	// 	tmp = tmp->next;
+	// }
+	// return (pibot);
+}
+void	divide_stack(t_node *a_node, t_node *b_node, int *pibot)
+{
+	t_list	*a_tmp;
+	int		size;
+	int		i;
+
+	i = 0;
+	size = a_node->size;
+	a_tmp = a_node->head;
+	while (i++ < size)
+	{
+		if (pibot[0] > a_tmp->data)
+			push_B(a_node, b_node);
+		else
+			rotate_a(a_node);
+		if (b_node->size >= 2 && pibot[1] > b_node->head->data)
+			rotate_b(b_node);
+		if (a_node->size == 3)
+			break ;
+	}
+	while (a_node->size != 3)
+		push_B(a_node, b_node);
+	three_list_sort(a_node);
+}
+
+int	find_a_location(int top, t_node *a_node)
+{
+	t_list	*tmp;
+	int		ind;
+	int		mind;
+
+	mind = 0;
+	ind = 0;
+	tmp = a_node->head->prev;
+	while (mind < a_node->size)
+	{
+		if (tmp->prev->data < tmp->data)
+		{
+			ind = mind;
+			break;
+		}
+		tmp = tmp->prev;
+		mind++;
+	}
+	while (ind < a_node->size)
+	{
+		if (top < tmp->data)
+			break ;
+		tmp = tmp->prev;
+		ind++;
+	}
+	if (ind >= (a_node->size + 1) / 2)
+		ind = (a_node->size - ind) * (-1);
+	return (ind);
+}
+
+int	location_cmp(int a, int b, int a_l, int b_l)
+{
+	if (b < 0)
+		b = -b;
+	if (b_l < 0)
+		b_l = -b_l;
+	if (a + b > a_l + b_l)
+		return (1);
+	return (0);
+}
+
+void	minimum_rotate(t_node *a_node, t_node *b_node, int *a, int *b)
+{
+	t_list	*tmp;
+	int		a_location;
+	int		b_location;
+	int		ind;
+	int		top;
+
+	ind = 0;
+	tmp = b_node->head->prev;
+	while (ind < b_node->size)
+	{
+		top = tmp->data;
+		b_location = ind;
+		a_location = find_a_location(top, a_node);
+		if (ind >= (b_node->size + 1) / 2)
+			b_location = (b_node->size - ind) * (-1);
+		if (ind == 0 || location_cmp(*a, *b, a_location, b_location))
+		{
+			*a = a_location;
+			*b = b_location;
+		}
+		tmp = tmp->prev;
+		ind++;
+	}
+}
+void	apply_rotate_r(t_node *a_node, t_node *b_node, int *a, int *b)
+{
+	while ((*a > 0) && (*b > 0))
+	{
+		rotate_r(a_node, b_node);
+		*b = *b - 1;
+		*a = *a - 1;
+	}
+	while ((*a < 0) && (*b < 0))
+	{
+		rev_rotate_r(a_node, b_node);
+		*b = *b + 1;
+		*a = *a + 1;
+	}
+}
+
+void	apply_rotate_a(t_node *a_node, int a)
+{
+	while (a > 0)
+	{
+		rotate_a(a_node);
+		a--;
+	}
+	while (a < 0)
+	{
+		rev_rotate_a(a_node);
+		a++;
+	}
+}
+
+void	apply_rotate_b(t_node *b_node, int b)
+{
+	while (b > 0)
+	{
+		rotate_b(b_node);
+		b--;
+	}
+	while (b < 0)
+	{
+		rev_rotate_b(b_node);
+		b++;
+	}
+}
+
+int	find_list_min(t_node *a_node, int *min_ind)
+{
+	t_list	*tmp;
+	int		min;
+
+	tmp = a_node->head->prev;
+	min = tmp->data;
+	while (*min_ind < a_node->size)
+	{
+		tmp = tmp->prev;
+		if (tmp->data < min)
+			min = tmp->data;
+		*min_ind = *min_ind + 1;
+	}
+	return (min);
+}
+
+void	sort_first(t_node *a_node)
+{
+	int	min;
+	int	min_ind;
+
+	min_ind = 0;
+	min = find_list_min(a_node, &min_ind);
+	if (min_ind >= (a_node->size + 1) / 2)
+		min_ind = (a_node->size - min_ind) * (-1);
+	while (min_ind)
+	{
+		if (min_ind > 0)
+		{
+			rotate_a(a_node);
+			min_ind--;
+		}
+		else
+		{
+			rev_rotate_a(a_node);
+			min_ind++;
+		}
+	}
+}
+
+void	sort(t_node *a_node, t_node *b_node)
+{
+	int		pibot[2];
+	int		a;
+	int		b;
+
+	find_pibot(a_node, pibot);
+	divide_stack(a_node, b_node, pibot);
+	ft_lstprint(a_node);
+	while (b_node->size)
+	{
+		a = 0;
+		b = 0;
+		minimum_rotate(a_node, b_node, &a, &b);
+		apply_rotate_r(a_node, b_node, &a, &b);
+		apply_rotate_a(a_node, a);
+		apply_rotate_b(b_node, b);
+		push_A(a_node, b_node);
+		ft_lstprint(a_node);
+
+	}
+	sort_first(a_node);
+}
+
 int main(int argc, char **argv)
 {
 	t_node	*a_node;
@@ -390,9 +668,6 @@ int main(int argc, char **argv)
 	if (arr_size == -1)
 		return (ERROR);
 
+	sort(a_node, b_node);
 	ft_lstprint(a_node);
-	// push_B(a_node, b_node);
-	five_list_sort(a_node, b_node);
-	ft_lstprint(a_node);
-	ft_lstprint(b_node);
 }
